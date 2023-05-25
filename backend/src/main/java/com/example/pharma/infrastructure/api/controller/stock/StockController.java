bdd566263cb350aed5caa4a25893fce86d06c515
Angel Espinosa <danielespinosa201517@gmail.com>
2023-05-25 16:50:43 -0500

    Venta y Traslado Terminada para Waldo Revisar

--- backend/src/main/java/com/example/pharma/infrastructure/api/controller/stock/StockController.java
+++ backend/src/main/java/com/example/pharma/infrastructure/api/controller/stock/StockController.java
@@ -1,12 +1,7 @@
 package com.example.pharma.infrastructure.api.controller.stock;
 
-import com.example.pharma.domain.entities.stock.Stock;
 import com.example.pharma.domain.service.stock.StockService;
-import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
